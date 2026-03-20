028b00bcd197a656af14e7b43a1a598434c5bcbf
qqwwalls <lentosik75@gmail.com>
2026-03-20 03:16:24 +0200

    Add missing domain entities changes

--- Shop.Domain/Entities/Order.cs
+++ Shop.Domain/Entities/Order.cs
@@ -1,12 +1,16 @@
-namespace Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
 
-public class Order
+namespace Shop.Domain.Entities
 {
-    public int Id { get; set; }
-    public int UserId { get; set; }
-    public User User { get; set; } = null!;
-    public DateTime OrderDate { get; set; }
-    public decimal TotalAmount { get; set; }
-    public string Status { get; set; } = string.Empty;
-    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = "New";
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
 }
--- Shop.Domain/Entities/Product.cs
+++ Shop.Domain/Entities/Product.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Shop.Domain.Entities;
-
-
-public class Product
+namespace Shop.Domain.Entities
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public decimal Price { get; set; }
-    public int StockQuantity { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public ICollection<CategoryProduct> CategoryProducts { get; set; } = new List<CategoryProduct>();
-    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public ICollection<CategoryProduct> CategoryProducts { get; set; } = new List<CategoryProduct>();
+    }
 }
